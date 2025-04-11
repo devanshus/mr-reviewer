dcead8584b5996e927580a799ce8135b3cd17a82
devanshu <devanshu.sharma@leegality.com>
2025-04-12 00:21:42 +0530

    first-changes

---
+++ app/src/test/java/dev/pr/reviewer/PrReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pr.reviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrReviewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
